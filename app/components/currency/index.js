08f18a9772a9ce9d87ebafa8333f166580a3d9f5
Xue hu <6681@ait.nsw.edu.au>
2019-06-14 20:16:05 +1000

    Home screen UI with animation

--- app/components/currency/index.js
+++ app/components/currency/index.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Logo from '../../assets/images/Logo.png'
+import { StyleSheet, Text, View, Image, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard  } from 'react-native';
+import { Logo } from './Logo'
+import  { InputWithButton } from './TextInput'
 
+const TEMP_BASE_CURRENCY = 'AUD';
+const TEMP_QUOTE_CURRENCY = 'USD';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '69.01';
+const TEMP_LAST_CONVERTED = new Date();
+const TEMP_CONVERSION_RATE = 0.79739;
 class CurrencyComponent extends Component {
+    handleChangeText = () => {
+        console.log('change text');
+    };
 
+    handlePressBaseCurrency = () => {
+        console.log('press base currency');
+    };
+
+    handlePressQuoteCurrency = () => {
+        console.log('press quote currency');
+    };
+
+    handleSwapCurrency = () => {
+        console.log('handle swap currency');
+    };
+
+    handleSwapCurrency = () => {
+        console.log('handle swap currency');
+    };
     render() {
         return (
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
             <View style={styles.container}>
-                <View style={styles.imgContainer}>
-                    <Image
-                        source={Logo}
-                        style={styles.image}
-                        resizeMode={'contain'} />
-                </View>
-                <Text> THis is the Currency </Text>
+                <KeyboardAvoidingView behavior="padding">
+                    <Logo />
+                    <InputWithButton
+                        buttonText={TEMP_BASE_CURRENCY}
+                        onPress={this.handlePressBaseCurrency}
+                        defaultValue={TEMP_BASE_PRICE}
+                        keyboardType="numeric"
+                        onChangeText={this.handleChangeText}
+                    />
+                    <InputWithButton
+                        editable={false}
+                        buttonText={TEMP_QUOTE_CURRENCY}
+                        onPress={this.handlePressQuoteCurrency}
+                        value={TEMP_QUOTE_PRICE}
+                    />
+                </KeyboardAvoidingView>
             </View>
+
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -24,7 +61,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#fff',
+        backgroundColor: '#e8cd1a',
     },
     imgContainer: {
         alignItems: 'center',
--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import { MainNavigator } from './router';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
 
 export default class App extends Component {
   render() {
@@ -18,4 +20,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+});
+
+
+EStyleSheet.build({
+  $primaryBlue: '#e8cd1a',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
+
+  $white: '#FFFFFF',
+  $lightGray: '#F0F0F0',
+  $border: '#E2E2E2',
+  $inputText: '#797979',
+  $darkText: '#343434',
 });
